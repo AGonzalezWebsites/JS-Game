ba902785f612a76a3f780efdfc96c40db4349e4a
AGonzalezWebsites <Agonzalez6259@yahoo.com>
2020-01-27 18:57:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -69,12 +69,10 @@ var changeName = function() {
 
 function inventoryToggleIn() {
     document.getElementById("inventoryAnimateIn").style.display = "inline-block";
-    document.getElementById("dialogBox").style.display = "none";
 }
 
 function inventoryToggleOut() {
     document.getElementById("inventoryAnimateIn").style.display = "none";
-    document.getElementById("dialogBox").style.display = "flex";
 }
 
 function topBoxToggle () {
